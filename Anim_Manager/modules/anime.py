3a7f609c317831c694147e15be6e12bbd9f199c7
Senu Gamer Boy 〽 <86920820+SenuGamerBoy@users.noreply.github.com>
2021-09-22 09:20:21 +0530

    Update anime.py

--- Anim_Manager/modules/anime.py
+++ Anim_Manager/modules/anime.py
@@ -359,6 +359,7 @@ Get information about anime, manga or characters with the help of this module! A
  - /character <character>: returns information about the character.
  - /manga <manga>: returns information about the manga.
  
+ 
  @senuinfinity
 """
 __mod_name__ = "Anime 👧"
