57d30dbbb6c1f87a739944b0614483334c439ec1
Senu Gamer Boy 〽 <86920820+SenuGamerBoy@users.noreply.github.com>
2021-09-22 09:19:40 +0530

    Update anime.py

--- Anim_Manager/modules/anime.py
+++ Anim_Manager/modules/anime.py
@@ -359,7 +359,7 @@ Get information about anime, manga or characters with the help of this module! A
  - /character <character>: returns information about the character.
  - /manga <manga>: returns information about the manga.
 """
-__mod_name__ = "ANIME SEARCH"
+__mod_name__ = "Anime 👧"
 
 
 AIRING_HANDLER = CommandHandler("airing", airing)
